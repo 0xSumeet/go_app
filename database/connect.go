eb7c9e7694eef0191695f99e463a369300f1c458
0xSumeet <techie.sam.dev.400@gmail.com>
2024-07-26 15:55:16 +0530

    database

---
+++ database/connect.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB(models ...interface{}) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s dbname=%s sslmode=disable password=%s port=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_PORT"),
+	)
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %s", err)
+	}
+	DB.AutoMigrate(models...)
+}
